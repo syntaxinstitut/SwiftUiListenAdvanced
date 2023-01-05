edc892dd27c4ff1389c503a12faf5bfe8abbd08e
Till Hemmerich <till.hemmerich@syntax-institut.de>
2023-01-05 09:06:08 +0100

    Initial Commit

---
+++ SwiftUiListenAdvanced/SwiftUiListenAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiListenAdvancedApp.swift
+//  SwiftUiListenAdvanced
+//
+//  Created by Till Hemmerich on 05.01.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiListenAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
